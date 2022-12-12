562ebd381fa70f3ec07ff64fdff450ba0ee95fa3
ibbidope <ibbidope@gmail.com>
2022-12-12 20:44:34 +0500

    modified:   task-tracker/package-lock.json 	modified:   task-tracker/package.json 	modified:   task-tracker/public/index.html 	deleted:    task-tracker/src/App.css 	modified:   task-tracker/src/App.js 	new file:   task-tracker/src/components/Header.js 	modified:   task-tracker/src/index.css 	modified:   task-tracker/src/index.js 	deleted:    task-tracker/src/logo.svg

---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Button color="primary">Submit</Button>
+    </header>
+  );
+};
+
+export default Header;
